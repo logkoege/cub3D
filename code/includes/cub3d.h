2cb575a248396b76e38412075d5d86b126245c2b
ubuntu <togokoegele@gmail.com>
2025-04-05 20:15:17 +0200

    start

---
+++ code/includes/cub3d.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: logkoege <logkoege@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 18:21:02 by logkoege          #+#    #+#             */
+/*   Updated: 2025/04/05 20:14:23 by logkoege         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <errno.h>
+# include <fcntl.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include "../../mlx_here/minilibx-linux/mlx.h"
+
+# define FLOOR			'0'
+# define WALL			'1'
+# define ITEM			'2'
+# define NORTH_CAMERA	'N'
+# define EAST_CAMERA	'E'
+# define WEST_CAMERA	'W'
+# define SOUTH_CAMERA	'S'
+
+
+
+
+# define ESC		65307
+# define W			119
+# define A			97
+# define S			115
+# define D			100
+
+
+typedef struct s_data
+{
+	
+}	t_data;
+
+typedef struct s_img
+{
+	
+}	t_img;
+
+#endif
